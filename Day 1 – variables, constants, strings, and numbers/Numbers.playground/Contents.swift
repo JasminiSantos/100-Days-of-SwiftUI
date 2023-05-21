ca3bff82eabd1074288f2aa36636b4de86b047af
jasminisantos <jasminirebeca10@gmail.com>
2023-05-20 22:56:33 -0300

    numbers

---
+++ Day 1 – variables, constants, strings, and numbers/Numbers.playground/Contents.swift
@@ -0,0 +1,34 @@
+
+let score = 10
+
+var reallyBig = 100000000
+
+// Swift doesn't care about underscores
+reallyBig = 100_000_000
+
+reallyBig = 1_00__00___00____00
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+print(score)
+
+var counter = 10
+counter = counter + 5
+
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))

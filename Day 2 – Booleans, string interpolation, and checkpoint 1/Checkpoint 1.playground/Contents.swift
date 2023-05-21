e979b7a54844c990f3d849fcbc424dca2e367bb2
jasminisantos <jasminirebeca10@gmail.com>
2023-05-21 18:57:18 -0300

    checkpoint 1

---
+++ Day 2 – Booleans, string interpolation, and checkpoint 1/Checkpoint 1.playground/Contents.swift
@@ -0,0 +1,5 @@
+
+let temperatureInCelsius = 27.0
+let temperatureInFahrenheit = temperatureInCelsius * 9/5 + 32
+print("Temperature in Celsius: \(temperatureInCelsius) °C")
+print("Temperature in Fahrenheit: \(temperatureInFahrenheit) °F")

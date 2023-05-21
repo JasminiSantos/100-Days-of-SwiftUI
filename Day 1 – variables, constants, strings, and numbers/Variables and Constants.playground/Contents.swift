e60fce5afe8a997b4be3cb5519784ac160b42127
jasminisantos <jasminirebeca10@gmail.com>
2023-05-20 22:34:27 -0300

    variables and constants

---
+++ Day 1 – variables, constants, strings, and numbers/Variables and Constants.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+// It is not possible to change constants
+//character = "Eloise"
+//character = "Francesca"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
